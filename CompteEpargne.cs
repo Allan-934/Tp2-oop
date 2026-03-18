06bcc78800dd9cd5f9fe0133a37706c8260c1869
Decugis Allan <allan.decugis@edouardgand.fr>
2026-03-18 16:48:28 +0100

    Partie 2 en cours

---
+++ CompteEpargne.cs
@@ -0,0 +1,12 @@
+public class CompteEpargne
+{
+    private int tauxInteret;
+    private int dateOuverture;
+
+    // Getters
+    public int GetTauxInteret() { return tauxInteret; }
+    public int GetDateOuverture() { return dateOuverture; }
+    // Setters
+    public void SetTauxInteret(int tauxInteret) { this.tauxInteret = tauxInteret; }
+    public void SetDateOuverture(int dateOuverture) { this.dateOuverture = dateOuverture; }
+}
---
+++ ComptePayant.cs
@@ -0,0 +1,12 @@
+public class ComptePayant
+{
+    private int commision;
+    private int nombreOperations;
+
+    // Getters
+    public int GetCommission() { return commision; }
+    public int GetNombreOperations() { return nombreOperations; }
+    // Setters
+    public void SetCommission(int commision) { this.commision = commision; }
+    public void SetNombreOperations(int nombreOperations) { this.nombreOperations = nombreOperations; }
+}
